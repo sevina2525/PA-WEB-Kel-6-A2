f40089ae7ed84f8dd16f67b10a8f3224ad48d816
sevina2525 <73829564+sevina2525@users.noreply.github.com>
2022-11-05 08:25:00 +0800

    Add files via upload

---
+++ jhacosmetics/add-product.php
@@ -0,0 +1,107 @@
+<?php 
+	session_start();
+	include 'config.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>JHA COSMETICS</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+	<!-- <script src="https://cdn.ckeditor.com/4.14.0/standard/ckeditor.js"></script> -->
+</head>
+<body>
+	<header>
+		<div class="container">
+			<h1><a href="index.php">JHA Cosmetics</a></h1>
+		</div>
+	</header>
+
+	<div class="section">
+		<div class="container">
+			<h3>Add Product Data</h3>
+			<div class="box">
+				<form action="" method="POST" enctype="multipart/form-data">
+					<select class="input-control" name="kategori" required>
+						<option value="">--Select--</option>
+						<?php 
+							$kategori = mysqli_query($conn, "SELECT * FROM tb_category ORDER BY category_id DESC");
+							while($r = mysqli_fetch_array($kategori)){
+						?>
+						<option value="<?php echo $r['category_id'] ?>"><?php echo $r['category_name'] ?></option>
+						<?php } ?>
+					</select>
+
+					<input type="text" name="nama" class="input-control" placeholder="Product Name" required>
+					<input type="text" name="harga" class="input-control" placeholder="Price" required>
+					<input type="file" name="gambar" class="input-control" required>
+					<input type="textarea" name="deskripsi" class="input-control" placeholder="Description"></textarea><br>
+					<select class="input-control" name="status">
+						<option value="">--Select--</option>
+						<option value="1">Ready</option>
+						<option value="0">Out of Stock</option>
+					</select>
+					<input type="submit" name="submit" value="Submit" class="btn">
+				</form>
+				<?php 
+					if(isset($_POST['submit'])){
+						$kategori 	= $_POST['kategori'];
+						$nama 		= $_POST['nama'];
+						$harga 		= $_POST['harga'];
+						$deskripsi 	= $_POST['deskripsi'];
+						$status 	= $_POST['status'];
+
+						$filename = $_FILES['gambar']['name'];
+						$tmp_name = $_FILES['gambar']['tmp_name'];
+
+						$type1 = explode('.', $filename);
+						$type2 = $type1[1];
+
+						$newname = 'images'.time().'.'.$type2;
+						$tipe_diizinkan = array('jpg', 'jpeg', 'png', 'gif');
+
+						if(!in_array($type2, $tipe_diizinkan)){
+							echo '<script>alert("File Format not allowed!")</scrtip>';
+
+						}else{
+							move_uploaded_file($tmp_name, './images/'.$newname);
+
+							$insert = mysqli_query($conn, "INSERT INTO tb_product VALUES (
+										null,
+										'".$kategori."',
+										'".$nama."',
+										'".$harga."',
+										'".$deskripsi."',
+										'".$newname."',
+										'".$status."',
+										null
+											) ");
+
+							if($insert){
+								echo '<script>alert("Add Data Succesfully!")</script>';
+								echo '<script>window.location="data-product.php"</script>';
+							}else{
+								echo 'Failes '.mysqli_error($conn);
+							}
+
+						}
+
+						
+						
+					}
+				?>
+			</div>
+		</div>
+	</div>
+	<footer>
+		<div class="container">
+			<small>Copyright &copy; 2022 - JHA Cosmetics</small>
+		</div>
+	</footer>
+	<script>
+        CKEDITOR.replace( 'deskripsi' );
+    </script>
+</body>
+</html>
---
+++ jhacosmetics/db_jhacosmetics.sql
@@ -0,0 +1,138 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 22, 2022 at 05:29 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.0.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_jhacosmetics`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_admin`
+--
+
+CREATE TABLE `tb_admin` (
+  `admin_id` int(11) NOT NULL,
+  `admin_name` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `admin_telp` varchar(20) NOT NULL,
+  `admin_email` varchar(50) NOT NULL,
+  `admin_address` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_category`
+--
+
+CREATE TABLE `tb_category` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_category`
+--
+
+INSERT INTO `tb_category` (`category_id`, `category_name`) VALUES
+(1, 'Lotions'),
+(2, 'Mascara'),
+(3, 'Skincare'),
+(4, 'Lipstick'),
+(11, 'Cosmetics'),
+(12, 'Powder');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_product`
+--
+
+CREATE TABLE `tb_product` (
+  `product_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `product_name` varchar(100) NOT NULL,
+  `product_price` int(11) NOT NULL,
+  `product_description` text NOT NULL,
+  `product_image` varchar(100) NOT NULL,
+  `product_status` tinyint(1) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_product`
+--
+
+INSERT INTO `tb_product` (`product_id`, `category_id`, `product_name`, `product_price`, `product_description`, `product_image`, `product_status`, `date_created`) VALUES
+(9, 3, 'Color Sky Cream', 75000, '<p>Ingredients :</p>', 'images1666451152.jpg', 0, '2021-11-05 02:57:20'),
+(10, 2, 'Mary Style Mascara', 60000, '<p>Ingredients :</p>', 'images1666451084.jpg', 1, '2021-11-05 02:59:39'),
+(11, 1, 'Ladies Lotion', 90000, '<p>Ingredients :</p>', 'images1666451021.jpg', 0, '2021-11-05 03:01:27'),
+(12, 4, 'Pink Lipstick', 87000, '<p>Ingredients :</p>\r\n', 'images1666447162.jpg', 1, '2021-11-05 03:03:59');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `tb_category`
+--
+ALTER TABLE `tb_category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `tb_product`
+--
+ALTER TABLE `tb_product`
+  ADD PRIMARY KEY (`product_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tb_category`
+--
+ALTER TABLE `tb_category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `tb_product`
+--
+ALTER TABLE `tb_product`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
