303137cfc12c25f4ad7b8b96a1a955c9610f910a
Nata De Coco <74284350+Natasya-Prajna@users.noreply.github.com>
2022-11-14 14:32:45 +0800

    Tampilan Multiuser Login

---
+++ cosmetic/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BETA TEST</title>
+    <link rel="stylesheet" type="text/css" href="indexstyle.css?v<?= time() ?>">
+</head>
+<body>
+    
+<h1>WELCOME
+    <br> JH COSMETICS
+</h1>
+    <div class="container">
+        <p align="CENTER"> LOGIN SEBAGAI :  </p>
+        <!-- <div class="pilihan">  -->
+        <!-- <form method="POST" action=""><br> -->
+        <ul>
+            <li>
+                <a href="admin/login.php"> Admin</a>
+                <!-- <input type="radio" name="MyRadio" value="First" checked>Admin<br> -->
+            </li>
+            <li>
+                <a href="user/login.php"> User</a>
+                <!-- <input type="radio" name="MyRadio" value="Second">User -->
+            </li>
+        </ul>
+       
+        
+        <br><br>
+        <!-- <input type="submit"  value="Result" name="Result">  -->
+        <!-- </div> -->
+    </div>
+    <!-- <button class="btn first" name="buton" value="admin">Admin</button>
+        <button class="btn first" name="buton" value="user">Pelanggan</button>
+        <button class="btn first" name="buton" value="">Close</button> -->
+    
+    <!-- </form> -->
+
+</body>
+</html>
